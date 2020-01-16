72b59a314dc8149f946913b6b10d1fabd895d576
Deepa Ann John <deepa.ann.john1@gmail.com>
2020-01-16 15:43:54 +0530

    circleci config.yml added

--- gatsby/deploy.sh
+++ gatsby/deploy.sh
@@ -1,3 +1,7 @@
+git config user.email "deepa.ann.john1@gmail.com"
+git config user.name "deepaannjohn"
 git checkout master
-# Add site files...
-git push
+cp gatsby/workspace/public/ 
+git add --all
+git commit -m "added public files"
+git push -u origin master
